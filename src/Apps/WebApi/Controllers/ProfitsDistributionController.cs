f5aba896c59e492e6af770526668ab3cd10c1955
andrew.braga_thera <andrew@thera.com.br>
2021-02-19 20:13:43 -0300

    Criação da estrutura do projeto

---
+++ src/Apps/WebApi/Controllers/ProfitsDistributionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.Common.Models;
+using Application.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfitsDistributionController : BaseApiController
+    {
+        private readonly ILogger<ProfitsDistributionController> _logger;
+
+        public ProfitsDistributionController(ILogger<ProfitsDistributionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResult<ProfitsDistributionDTO>>> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
