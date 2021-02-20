33ee9b366686451199168d98f7983ddb27e361b1
andrew.braga_thera <andrew@thera.com.br>
2021-02-20 13:22:43 -0300

    Correção nos nomes da classes

---
+++ src/Apps/WebApi/Controllers/ProfitsSharingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Application.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfitsSharingController : BaseApiController
+    {
+        private readonly ILogger<ProfitsSharingController> _logger;
+
+        public ProfitsSharingController(ILogger<ProfitsSharingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<ProfitsSharingDTO> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
